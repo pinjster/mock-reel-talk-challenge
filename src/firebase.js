2dd7c716920105b0de9b5fd5178477113170cdbb
pinjster <pinj2000@gmail.com>
2023-10-24 03:09:55 -0400

    removed webpack, added nodemon and express

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -14,4 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import express from 'express';
+ 
+
+const app: express.Application = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+ 
+const port: number = 3000;
+ 
+app.get('/', (_req, _res) => {
+    _res.send("TypeScript With Express");
+});
+
+app.post('/api/customers', (_req, _res) => {
+    _res.send('This is a post that is cool')
+})
+
+app.listen(port, () => {
+    console.log(`TypeScript with Express 
+         http://localhost:${port}/`);
+});
