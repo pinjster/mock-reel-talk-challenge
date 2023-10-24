b7b2cbc9283cc5efcf63e7563fb3af5b1e630fe6
pinjster <pinj2000@gmail.com>
2023-10-23 23:59:30 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJZ0G0ca5aXoGvCvpH4tN31LwrCCQIvjo",
+  authDomain: "mock-reel-talk-challenge.firebaseapp.com",
+  projectId: "mock-reel-talk-challenge",
+  storageBucket: "mock-reel-talk-challenge.appspot.com",
+  messagingSenderId: "225216224296",
+  appId: "1:225216224296:web:e963cf919b1ee7a852a91d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
